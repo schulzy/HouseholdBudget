bada1a17c29b596cc8b29be23ad83be5be609efc
schulzy <schulzyy@gmail.com>
2021-05-30 23:33:14 +0200

    Add new unittest project

---
+++ Tests/Schulteisz.Document.PdfReader.Test/Create_new_Document.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Schulteisz.Document.Interfaces;
+using System;
+
+namespace Schulteisz.Document.PdfReader
+{
+    [TestClass]
+    public class Create_new_Document
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "A document creation with empty parameter was inappropriately allowed.")]
+        public void With_EmptyString_Argument()
+        {
+            IDocument document = new Document(string.Empty);
+        }
+    }
+}
