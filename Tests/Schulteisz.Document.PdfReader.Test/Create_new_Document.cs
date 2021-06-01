bd5d9132b2bfebd87d09591043dbd065d1898752
schulzy <schulzyy@gmail.com>
2021-06-01 22:49:11 +0200

    Add tests

--- Tests/Schulteisz.Document.PdfReader.Test/Create_new_Document.cs
+++ Tests/Schulteisz.Document.PdfReader.Test/Create_new_Document.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Schulteisz.Document.Interfaces;
 using System;
+using System.IO;
+using UglyToad.PdfPig.Core;
 
 namespace Schulteisz.Document.PdfReader
 {
@@ -12,6 +14,24 @@ namespace Schulteisz.Document.PdfReader
         public void With_EmptyString_Argument()
         {
             IDocument document = new Document(string.Empty);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "A document creation with wrong path parameter was inappropriately allowed.")]
+        public void With_Wrong_Path()
+        {
+            var baseDir = AppDomain.CurrentDomain.BaseDirectory;
+            var pdfPath = Path.Combine(baseDir, "notExisting.pdf");
+            IDocument document = new Document(pdfPath);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(PdfDocumentFormatException), "A document creation with wrong file parameter was inappropriately allowed.")]
+        public void With_Wrong_File()
+        {
+            var baseDir = AppDomain.CurrentDomain.BaseDirectory;
+            var pdfPath = Path.Combine(baseDir, "TestData", "dummy.txt");
+            IDocument document = new Document(pdfPath);
         }
     }
 }
